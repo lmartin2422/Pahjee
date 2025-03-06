40bc9d6e6fb7d76b56946c066768c62eafd472c3
lmartin2422 <lmartin2422@gmail.com>
2025-03-05 21:13:40 -0500

    changes to the message page. added profile icon to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Import RouterModule
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule], // Add RouterModule to imports
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
